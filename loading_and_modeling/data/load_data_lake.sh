cc114c91fa16c2bd3fe26743f454ca91d05b3deb
w205 <w205@ip-172-31-36-179.ec2.internal>
2017-08-02 03:52:45 +0000

    Add existing file

---
+++ loading_and_modeling/data/load_data_lake.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Rename csv files and load them into  HDFS
+
+# hdfs dfs -mkdir /user/w205/hopsital_compare;
+
+# unzip hospital files
+
+unzip /home/w205/w205_Exercise_1/loading_and_modeling/data/HCAHPS_-_Hospital.csv.zip
+
+unzip /home/w205/w205_Exercise_1/loading_and_modeling/data/Timely_and_Effective_Care_-_Hospital.csv.zip
+
+# Rename, Remove Header, place in HDFS Simultaneously 
+tail -n +2 /home/w205/w205_Exercise_1/loading_and_modeling/data/HCAHPS_-_Hospital.csv > /home/w205/w205_Exercise_1/loading_and_modeling/data/Survey_Hospital.csv;
+hdfs dfs -put /home/w205/w205_Exercise_1/loading_and_modeling/data/Survey_Hospital.csv /user/w205/hospital_compare;
+
+tail -n +2 /home/w205/w205_Exercise_1/loading_and_modeling/data/HCAHPS_-_State.csv > /home/w205/w205_Exercise_1/loading_and_modeling/data/Survey_State.csv;
+hdfs dfs -put /home/w205/w205_Exercise_1/loading_and_modeling/data/Survey_State.csv /user/w205/hospital_compare;
+
+tail -n +2 /home/w205/w205_Exercise_1/loading_and_modeling/data/Timely_and_Effective_Care_-_Hospital.csv > /home/w205/w205_Exercise_1/loading_and_modeling/data/Scores_Hospital.csv;
+hdfs dfs -put /home/w205/w205_Exercise_1/loading_and_modeling/data/Scores_Hospital.csv /user/w205/hospital_compare;
+
+tail -n +2 /home/w205/w205_Exercise_1/loading_and_modeling/data/Timely_and_Effective_Care_-_State.csv > /home/w205/w205_Exercise_1/loading_and_modeling/data/Scores_State.csv;
+hdfs dfs -put /home/w205/w205_Exercise_1/loading_and_modeling/data/Scores_State.csv /user/w205/hospital_compare;
